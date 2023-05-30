c2ff97ad043e2a62b7ef3d2eaed00291f3ad1156
windcannons <2717741632@qq.com>
2023-05-30 19:11:46 +0800

    刘超补全vue文件

---
+++ manage/src/views/UserInfoView.vue
@@ -0,0 +1,27 @@
+<template>
+<!--用户信息-->
+  <div class="UserInfoView">
+      用户信息
+  </div>
+</template>
+
+<script>
+export default {
+    name: "UserInfoView"
+}
+</script>
+
+<style
+    scoped lang="less">
+.UserInfoView{
+    width: 100%;
+    height: 100vh;
+    background-color: #fff;
+    z-index: 9;
+    position: fixed;
+    top: 0;
+    left: 0;
+}
+</style>
+
+
