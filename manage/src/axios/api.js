eeac8ddd505c01589557414c1995339490dcaa60
windcannons <2717741632@qq.com>
2023-06-02 12:59:57 +0800

    刘超第五次上传模板

--- manage/src/axios/api.js
+++ manage/src/axios/api.js
@@ -1,18 +1,35 @@
 // 对请求接口进行封装
 
 // 引入封装好的axios实例
-import requests from './axios.js';
+import requests
+    from './axios.js';
 
 // 通知管理 start
-let notice = (params={})=>{
-  return requests.get('/notice/page',params)
+let notice = (currentPage = 1, size = 10) => {
+    return requests.get(`/notice/page?currentPage=${currentPage}&size=${size}`)
+}
+let delnotice = (id) => {
+    return requests.post('/notice/delete?nicId=' + id)
+}
+let addOrUpdate = (params = {}) => {
+    return requests.post(`/notice/addOrUpdate`, params)
 }
 
 // 通知管理 end
+
 // 登录start
-let login = (params={})=>{
-  return requests.post('/login',params)
+let login = (params = {}) => {
+    return requests.post('/login', params)
+}
+let adminInfo = (params = {}) => {
+    return requests.get('/admin/info', params)
 }
 // 登录end
 
-export {notice,login}
+export {
+    notice,
+    login,
+    delnotice,
+    addOrUpdate,
+    adminInfo
+}
